99b74e8ee9ae3454cfde3bbdca44110340fe06a2
Albert Gabdullin <albert.gabdullin@piano.io>
2019-06-30 01:07:05 +0300

    update files in app folder

--- app/js/common.js
+++ app/js/common.js
@@ -58,15 +58,15 @@ $(function() {
 				if(index === 5 && ballAnimation) {
 					var wow1 = new WOW ();
 					wow1.init();
-					ballAnimation = false
+					ballAnimation = false;
 				}
 			},   // This option accepts a callback function. The function will be called after the page moves.
 			loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
 			keyboard: true,                  // You can activate the keyboard controls
 			responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-																			 // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-																			 // the browser's width is less than 600, the fallback will kick in.
-			direction: "horizontal"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
+			// you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+			// the browser's width is less than 600, the fallback will kick in.
+			direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
 		});
 
 		$('.scroll-link').on('click', function() {
@@ -104,13 +104,19 @@ $(function() {
 			var _href = $(this).attr("href");
 			$("html, body").animate({scrollTop: $(_href).offset().top - 56 +"px"});
 			return false;
-	});
+		});
 		$("nav.mobile .button--top").click(function() {
 			var _href = $(this).attr("href");
 			$("html, body").animate({scrollTop: $(_href).offset().top - 116 +"px"});
 			return false;
 		});
-}
+
+		$('.ball__item').click(function(){
+			var _href = $(this).attr("href");
+			$("html, body").animate({scrollTop: $(_href).offset().top - 56 +"px"});
+			return false;
+		});
+	}
 
 	var wow = new WOW(
 		{
@@ -119,147 +125,147 @@ $(function() {
 	);
 	wow.init();
 
-$('.ball__item').click(function(e) {
-	if(!$(this).hasClass('ball__item--active')) {
-		e.preventDefault();
-		$('.main').moveTo(9);
-	}
-});
+	$('.ball__item').click(function(e) {
+		if(!$(this).hasClass('ball__item--active')) {
+			e.preventDefault();
+			$('.main').moveTo(9);
+		}
+	});
 
-$('nav.mobile ul li a, nav.mobile .button').on('click', function(){
-	$('.menu-btn').removeClass('menu-btn--active');
-	$('nav.mobile').removeClass('active');
-});
+	$('nav.mobile ul li a, nav.mobile .button').on('click', function(){
+		$('.menu-btn').removeClass('menu-btn--active');
+		$('nav.mobile').removeClass('active');
+	});
 
 
 	//swipe button code
 	var swiperDragged = false,
-        startX,
-        endX = 0;
+		startX,
+		endX = 0;
 
-    function swipe(){
-        var $swipe = $('.swiper'),
-            $btn = $('.swipe-btn', $swipe);
+	function swipe(){
+		var $swipe = $('.swiper'),
+			$btn = $('.swipe-btn', $swipe);
 
-    TweenLite.to('#swipe-arrow', 0, { x: 16, y: 11});
-    TweenLite.to('#swipe-end', 0, { x: 235, y: 12});
+		TweenLite.to('#swipe-arrow', 0, { x: 16, y: 11});
+		TweenLite.to('#swipe-end', 0, { x: 235, y: 12});
 
-    var tl = new TimelineMax({ repeat: -1 });
-    tl.staggerFrom("#dotted-line circle", 0.7, { scale: 0.6, x: -2, y: .5, opacity: 0.7, delay:0.1, ease: Power2.easeInOut, repeat: 1, yoyo: true}, 0.15);
+		var tl = new TimelineMax({ repeat: -1 });
+		tl.staggerFrom("#dotted-line circle", 0.7, { scale: 0.6, x: -2, y: .5, opacity: 0.7, delay:0.1, ease: Power2.easeInOut, repeat: 1, yoyo: true}, 0.15);
 
-    $btn.on('click touchend', function(e){
-        e.preventDefault();
-    }).on('touchstart mousedown', function(e) {
-        e.preventDefault();
-        swiperDragged = true;
-        startX = typeof e.pageX != 'undefined' ? e.pageX : e.originalEvent.touches[0].pageX;
-        endX = 0;
-    });
+		$btn.on('click touchend', function(e){
+			e.preventDefault();
+		}).on('touchstart mousedown', function(e) {
+			e.preventDefault();
+			swiperDragged = true;
+			startX = typeof e.pageX != 'undefined' ? e.pageX : e.originalEvent.touches[0].pageX;
+			endX = 0;
+		});
 
-    $(document).on('touchmove mousemove', function(e){
-        if (swiperDragged) {
-            actualX = typeof e.pageX != 'undefined' ? e.pageX : e.originalEvent.touches[0].pageX;
-            endX = Math.max(0, Math.min(215, actualX - startX));
-            TweenLite.to('#swipe-btn', 0, { x: endX});
-        }
-    }).on('touchend mouseup', function(e) {
-				if (swiperDragged) {
-						swiperDragged = false;
-						if (endX < 200) {
-								//TweenLite.to('#swipe-btn', .5, { x: 0 });
-						} else {
-								// console.log($btn.attr('xlink:href'));
-								TweenLite.to('#swipe-btn', .1, { x: 215});
-								setTimeout(function(){
-										TweenLite.to('#swipe-btn', .5, { x: 0 });
-										$('html, body').removeClass('not-active');
-										$('header').addClass('active');
-										$('.first-section').fadeOut(100);
+		$(document).on('touchmove mousemove', function(e){
+			if (swiperDragged) {
+				actualX = typeof e.pageX != 'undefined' ? e.pageX : e.originalEvent.touches[0].pageX;
+				endX = Math.max(0, Math.min(215, actualX - startX));
+				TweenLite.to('#swipe-btn', 0, { x: endX});
+			}
+		}).on('touchend mouseup', function(e) {
+			if (swiperDragged) {
+				swiperDragged = false;
+				if (endX < 200) {
+					//TweenLite.to('#swipe-btn', .5, { x: 0 });
+				} else {
+					// console.log($btn.attr('xlink:href'));
+					TweenLite.to('#swipe-btn', .1, { x: 215});
+					setTimeout(function(){
+						TweenLite.to('#swipe-btn', .5, { x: 0 });
+						$('html, body').removeClass('not-active');
+						$('header').addClass('active');
+						$('.first-section').fadeOut(100);
 
-										new WOW().init();
-								}, 0);
-						}
-					endX = 0;
+						new WOW().init();
+					}, 0);
 				}
-      });
-    }
+				endX = 0;
+			}
+		});
+	}
 
-    swipe();
-		//===swipper
+	swipe();
+	//===swipper
 
 
 
 
 //burger btn code
-$('.menu-btn').on('click', function() {
-	$(this).toggleClass('menu-btn--active');
-	if($('nav.mobile').hasClass('active')) {
-		$('nav.mobile').removeClass('active');
-	}
-	else {
-		$('nav.mobile').addClass('active');
-	}
-});
+	$('.menu-btn').on('click', function() {
+		$(this).toggleClass('menu-btn--active');
+		if($('nav.mobile').hasClass('active')) {
+			$('nav.mobile').removeClass('active');
+		}
+		else {
+			$('nav.mobile').addClass('active');
+		}
+	});
 
-$('.play-button').on('click', function() {
-	$('.video__modal').addClass('active');
-	$('#video')[0].play();
-});
+	$('.play-button').on('click', function() {
+		$('.video__modal').addClass('active');
+		$('#video')[0].play();
+	});
 
-$('.video__modal-close').on('click', function(){
-	$('#video')[0].pause();
-});
-
-
-$('.compose__img').tilt({
-	maxTilt:        10,
-	perspective:    1000,
-	easing:         "cubic-bezier(.03,.98,.52,.99)",
-	scale:          1,
-	speed:          300,
-	transition:     true,
-	disableAxis:    null,
-	reset:          true,
-	glare:          false,
-	maxGlare:       1,
-});
+	$('.video__modal-close').on('click', function(){
+		$('#video')[0].pause();
+	});
 
 
-var success_modal = $('.success__modal');
+	$('.compose__img').tilt({
+		maxTilt:        10,
+		perspective:    1000,
+		easing:         "cubic-bezier(.03,.98,.52,.99)",
+		scale:          1,
+		speed:          300,
+		transition:     true,
+		disableAxis:    null,
+		reset:          true,
+		glare:          false,
+		maxGlare:       1,
+	});
 
-$('.close__btn').on('click', function() {
-	success_modal.removeClass('active');
-});
 
-$(document).not(success_modal).click(function(){
-	success_modal.removeClass('active');
-});
+	var success_modal = $('.success__modal');
+
+	$('.close__btn').on('click', function() {
+		success_modal.removeClass('active');
+	});
+
+	$(document).not(success_modal).click(function(){
+		success_modal.removeClass('active');
+	});
 //forms
 
-$('.form1').submit(function () {
-	return false;
-}).validate({
-	rules : {
+	$('.form1').submit(function () {
+		return false;
+	}).validate({
+		rules : {
 			"name" : {required : true, minlength: 1},
 			"phone" : {required : true, minlength: 3},
 			"agree" : {required : true},
-	},
-	messages : {
-		"name" : {
-					required : "Пожалуйста введите имя",
-					minlength : "Не менее одной буквы"
+		},
+		messages : {
+			"name" : {
+				required : "Пожалуйста введите имя",
+				minlength : "Не менее одной буквы"
 			},
-		"phone" : {
+			"phone" : {
 				required : "Пожалуйста введите телефон",
 				minlength : "Не менее 10 цифр"
-		},
-		"agree" : {
+			},
+			"agree" : {
 				required : "Пожалуйста примимте соглашение",
-		}
+			}
 
-	},
-	submitHandler: function(form){
-		$.ajax({
+		},
+		submitHandler: function(form){
+			$.ajax({
 				url:'/mail.php',
 				data: $(form).serialize(),
 				type: 'POST',
@@ -270,42 +276,42 @@ $('.form1').submit(function () {
 						$('.success__modal').removeClass('active');
 					})
 				}
-		});
+			});
 
-	},
-	errorPlacement: function (error, element) {
+		},
+		errorPlacement: function (error, element) {
 			$(element).siblings().last().after(error.addClass('f-help f-help-error'));
-	},
-	errorElement : 'div'
-});
+		},
+		errorElement : 'div'
+	});
 
-$('.main-form').submit(function () {
-	return false;
-}).validate({
-	rules : {
+	$('.main-form').submit(function () {
+		return false;
+	}).validate({
+		rules : {
 			"name" : {required : true, minlength: 1},
 			"phone" : {required : true, minlength: 3},
 			"club_name" : {required : false, minlength: 3},
 			"email" : {required : false, minlength: 3},
 			"role" : {required : false, minlength: 3},
 			"agree1" : {required : true},
-	},
-	messages : {
-		"name" : {
-					required : "Пожалуйста введите имя",
-					minlength : "Не менее одной буквы"
+		},
+		messages : {
+			"name" : {
+				required : "Пожалуйста введите имя",
+				minlength : "Не менее одной буквы"
 			},
-		"phone" : {
+			"phone" : {
 				required : "Пожалуйста введите телефон",
 				minlength : "Не менее 10 цифр"
-		},
-		"agree1" : {
+			},
+			"agree1" : {
 				required : "Пожалуйста примимте соглашение",
-		}
+			}
 
-	},
-	submitHandler: function(form){
-		$.ajax({
+		},
+		submitHandler: function(form){
+			$.ajax({
 				url:'/mail.php',
 				data: $(form).serialize(),
 				type: 'POST',
@@ -315,39 +321,39 @@ $('.main-form').submit(function () {
 
 
 				}
-		});
-	},
-	errorPlacement: function (error, element) {
+			});
+		},
+		errorPlacement: function (error, element) {
 			$(element).siblings().last().after(error.addClass('f-help f-help-error'));
-	},
-	errorElement : 'div'
-});
+		},
+		errorElement : 'div'
+	});
 
-$("#modal__360-button").animatedModal({
-	color: '#000',
-	animatedIn: 'fadeIn',
-	animatedOut: 'fadeOut',
-	beforeOpen: function(){
+	$("#modal__360-button").animatedModal({
+		color: '#000',
+		animatedIn: 'fadeIn',
+		animatedOut: 'fadeOut',
+		beforeOpen: function(){
 			$('.modal__360-content').html('');
 			var fam = $('#family').val();
 			var player = $('#player').val();
-			var url = `http://zenit-kazan.com/youbecameteam/?fam=${fam}`;
+			var url = `http://zenit.360ticket.org?fam=${fam}&number=${player}`;
 			$('.modal__360-content').html(`
 					<iframe src="${url}" allowFullScreen frameborder="0"></iframe>
 			`);
-	}
-});
+		}
+	});
 
 
-$('#modal__360-button').click(function(){
-	$('.modal__360').css('display', 'block');
-});
+	$('#modal__360-button').click(function(){
+		$('.modal__360').css('display', 'block');
+	});
 
 //year detect
-var today = new Date();
-var year = today.getFullYear();
+	var today = new Date();
+	var year = today.getFullYear();
 
-$('.now-year').text(year);
+	$('.now-year').text(year);
 
 //menu init
 // $("nav.desktop ul li a").each(function() {
@@ -369,16 +375,3 @@ $('.now-year').text(year);
 // });
 
 });
-
-
-
-
-
-
-// VanillaTilt.init(document.querySelectorAll(".compose__img"), {
-// 	max: 20,
-// 	speed: 300,
-// 	perspective: 1000,
-// 	easing: "cubic-bezier(.03,.98,.52,.99)",
-// 	scale: 1,
-// });
